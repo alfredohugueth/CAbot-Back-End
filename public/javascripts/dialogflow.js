d5490bb519075f7c6d05d43feb2709597d11160a
alfredohugueth <alfredohugueth99@gmail.com>
2021-03-03 16:00:09 -0500

    Communication dialogflowAPI-backend-frontend Complete for text entry

---
+++ public/javascripts/dialogflow.js
@@ -0,0 +1,54 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const config = require("./config");
+
+const credentials = {
+  client_email: config.GOOGLE_CLIENT_EMAIL,
+  private_key: config.GOOGLE_PRIVATE_KEY,
+};
+// El keyFilename tiene que ser la ruta hacia tu cuenta de servicio generada.
+// The keyFilename have to be the path to your service account of google.
+const sessionClient = new dialogflow.SessionsClient({
+  keyFilename:"./public/javascripts/cata-bgij-8b3eb445ba46.json"
+});
+
+/**
+ * Send a query to the dialogflow agent, and return the query result.
+ * @param {string} projectId The project to be used
+ */
+async function sendToDialogFlow(msg, session, source, params) {
+  let textToDialogFlow = msg;
+  sess = "123123"
+  try {
+    const sessionPath = sessionClient.projectAgentSessionPath(
+      config.GOOGLE_PROJECT_ID,
+      session
+    );
+
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: textToDialogFlow,
+          languageCode: config.DF_LANGUAGE_CODE,
+        }
+      },
+      queryParams: {
+        payload: {
+          data: "params",
+        },
+      }
+    };
+      const responses = await sessionClient.detectIntent(request);
+      const result = responses;
+      return result
+    }catch(error){
+        console.log(error);
+        
+    }
+    
+}
+
+module.exports = {
+    sendToDialogFlow,
+  };
+  
--- routes/mensajes.js
+++ routes/mensajes.js
@@ -1,14 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var dialogflow = require("../public/javascripts/dialogflow.js");
 
 // Primer mensaje del servidor.
 router.get('/',(req,res)=>{
     console.log('Ingreso nuevo usuario a la pagina, enviar primer mensaje del bot.');
     let msgBienve = 'Hola nuevo usuario, mi nombre es CAbot tu asistente de control automatico';
-    let botmsg = {
+    let msgEnviado = {
+        "boot":{
+        "estado":true,
         "texto":msgBienve
-    }
-    res.json(botmsg);
+      },
+      user:{
+        estado:false,
+        texto:''
+      }}
+    
+    res.json(msgEnviado);
+});
+
+router.post('/usuario',async(req,res)=>{
+
+
+    console.log('Mensaje de usuario recibido...');
+    let msgUsuario = req.body.texto;
+    let userID = req.body.userID;
+    // Una vez obtenemos el id para identificar al usuario, realizamos el siguiente cambio.
+    // Consultamos a dialogflow la respuesta a la pregunta realizada por el usuario.
+    let respuesta = await dialogflow.sendToDialogFlow(msgUsuario,userID,"Angular");
+    let mensajeRecibido = {
+        "boot":{
+        "estado":true,
+        "texto":respuesta[0].queryResult.fulfillmentText,
+        "voz": respuesta[0].outputAudio,
+        "configAudio": respuesta[0].outputAudioConfig
+      },
+      user:{
+        estado:false,
+        texto:''
+      }}
+      console.log(mensajeRecibido);
+    // Mandamos el mensaje json recibido en el formato establecido por el front de angular
+    res.json(mensajeRecibido);
 });
 
 
